a780484dd72b5b60e1b88175884e60eabd4c8a70
Mikhail 'stv233' Glazatov <glazatov.mihail@gmail.com>
2020-05-31 17:18:40 +0700

    Добавлено меню

--- Inventory editor/Form1.Designer.cs
+++ Inventory editor/Form1.Designer.cs
@@ -28,23 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.ImsMainMenu = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ImsMainMenu.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // ImsMainMenu
+            // 
+            this.ImsMainMenu.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.ImsMainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.ImsMainMenu.Location = new System.Drawing.Point(0, 0);
+            this.ImsMainMenu.Name = "ImsMainMenu";
+            this.ImsMainMenu.Size = new System.Drawing.Size(1223, 28);
+            this.ImsMainMenu.TabIndex = 0;
+            this.ImsMainMenu.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "File";
             // 
             // IfrMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1223, 666);
+            this.Controls.Add(this.ImsMainMenu);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.ImsMainMenu;
             this.Name = "IfrMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Inventory edittor";
             this.UseWaitCursor = true;
+            this.ImsMainMenu.ResumeLayout(false);
+            this.ImsMainMenu.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip ImsMainMenu;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
     }
 }
 
